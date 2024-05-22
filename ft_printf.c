3b66e3121569eba45495ab91fc78bb823af20b32
Nick Boer <nboer@c3a5c1.42berlin.de>
2024-05-22 18:02:59 +0200

    yes

---
+++ ft_printf.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nboer <nboer@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/22 14:55:35 by nboer             #+#    #+#             */
+/*   Updated: 2024/05/22 17:45:52 by nboer            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_printf(const char *, ...)
+
+//TO-DO Create a series of argument if statements to allow for different conversions:
+if (ARGUMENT == c)
+else if (ARGUMENT == s)
+else if (ARGUMENT == p)
+else if (ARGUMENT == d)
+else if (ARGUMENT == i) 
+else if (ARGUMENT == u)
+else if (ARGUMENT == x)
+else if (ARGUMENT = x)
+else ...
+
